cc3e9aa13cf367bbc5c3da6c1f5d12440e0df817
Luicil <luicilfernandes@hotmail.com>
2022-02-10 16:20:28 -0300

    Aula 94

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,14 +1,35 @@
 import React from "react";
-import{View, Text, TextInput} from "react-native";
+import{View, TextInput, StyleSheet} from "react-native";
 
 import FormRow from "../components/FormRow";
 
 export default class LoginScreen extends React.Component{
     render(){
         return(
-            <FormRow>
-                <TextInput />
-            </FormRow>
+            <view>
+                <FormRow>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="user@mail.com"
+                    />
+                </FormRow>
+                <FormRow>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="*****"
+                        //secureTextEntry={true}
+                        secureTextEntry
+                    />
+                </FormRow>
+            </view>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    input: {
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 10,
+    }
+})
